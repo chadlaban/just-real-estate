46f52b95557e2609b7d710b572c6880ef36dbca1
Chad <laban.chad@gmail.com>
2025-03-31 02:05:27 +0800

    Add CORS, initial methods, and CRON job

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,38 @@
-require('dotenv').config();
-const express = require('express');
+import "dotenv/config";
+import "./config/database.js";
+import express from "express";
+import cors from "cors";
+import propertyRoutes from "./routes/property.routes.js";
+// import cron from "node-cron";
+// import importProperties from "./controllers/property.controller.js";
+
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.URL + process.env.CLIENT_PORT,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Backend is running!');
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
 });
+
+app.use("/api", propertyRoutes);
+
+// fetch and import data
+// cron.schedule("* * * * *", async () => {
+//   try {
+//     console.log("Running importProperties job...");
+//     await importProperties();
+//   } catch (error) {
+//     console.error("Error in importProperties job:", error);
+//   }
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
