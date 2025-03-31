98a2947db97847896f33bf283d4c5ab47823337c
Chad <laban.chad@gmail.com>
2025-03-31 22:30:22 +0800

    Add update and delete routes

--- backend/routes/property.routes.js
+++ backend/routes/property.routes.js
@@ -2,11 +2,15 @@ import express from "express";
 import {
   getProperties,
   addProperty,
+  updateProperty,
+  deleteProperty,
 } from "../controllers/property.controller.js";
 
 const router = express.Router();
 
 router.get("/properties", getProperties);
 router.post("/properties/create", addProperty);
+router.put("/properties/update/:id", updateProperty);
+router.delete("/properties/delete/:id", deleteProperty);
 
 export default router;
