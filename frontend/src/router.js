0ec1312e798cda55807662fa8e088dcdca03ecec
Chad <laban.chad@gmail.com>
2025-03-31 22:26:49 +0800

    Adding route for Management page

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import LandingPage from "./views/LandingPage.vue";
 import RealEstateListings from "./views/RealEstateListings.vue";
 import About from "./views/About.vue";
+import Manage from "./views/management/Manage.vue";
 
 const routes = [
   { path: "/", component: LandingPage },
   { path: "/real-estate-listings", component: RealEstateListings },
   { path: "/about-us", component: About },
+  { path: "/management", component: Manage },
 ];
 
 const router = createRouter({
