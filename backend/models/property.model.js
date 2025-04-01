1d3abb71d018d1bbf3996783664d4831d7dee42c
Chad <laban.chad@gmail.com>
2025-04-01 20:57:51 +0800

    Fix models to ESM

--- backend/models/property.model.js
+++ backend/models/property.model.js
@@ -1,29 +1,124 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/database.js";
+import User from "./user.model.js";
 
-const Property = sequelize.define("Property", {
-  formatted_address: DataTypes.STRING,
-  address_line1: DataTypes.STRING,
-  address_line2: DataTypes.STRING,
-  city: DataTypes.STRING,
-  state: DataTypes.STRING,
-  zip_code: DataTypes.STRING,
-  county: DataTypes.STRING,
-  latitude: DataTypes.FLOAT,
-  longitude: DataTypes.FLOAT,
-  property_type: DataTypes.STRING,
-  bedrooms: DataTypes.INTEGER,
-  bathrooms: DataTypes.FLOAT,
-  square_footage: DataTypes.INTEGER,
-  lot_size: DataTypes.INTEGER,
-  year_built: DataTypes.INTEGER,
-  assessor_id: DataTypes.STRING,
-  legal_description: DataTypes.STRING,
-  subdivision: DataTypes.STRING,
-  last_sale_date: DataTypes.DATE,
-  created_by: DataTypes.STRING,
-  deleted_at: DataTypes.DATE,
-  deleted_by: DataTypes.DATE,
+const Properties = sequelize.define("Properties", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: "Users",
+      key: "id",
+    },
+    onUpdate: "CASCADE",
+    onDelete: "SET NULL",
+  },
+  formatted_address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  address_line1: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  address_line2: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  state: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  zip_code: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  county: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  latitude: {
+    type: DataTypes.FLOAT,
+    allowNull: true,
+  },
+  longitude: {
+    type: DataTypes.FLOAT,
+    allowNull: true,
+  },
+  property_type: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  bedrooms: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  bathrooms: {
+    type: DataTypes.FLOAT,
+    allowNull: true,
+  },
+  square_footage: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  lot_size: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  year_built: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  assessor_id: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  legal_description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  subdivision: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  last_sale_date: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  created_by: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  deleted_at: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  deleted_by: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
 });
 
-export default Property;
+Properties.belongsTo(User, { foreignKey: "userId" });
+
+export default Properties;
---
+++ backend/models/user.model.js
@@ -0,0 +1,46 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+// import Properties from "./property.model.js";
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// User.hasMany(Properties, { foreignKey: "userId" });
+
+export default User;
