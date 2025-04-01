b15946ace6e64f90dbd47d1b4c50d0bdd01e3dd9
Chad <laban.chad@gmail.com>
2025-04-01 18:21:05 +0800

    Add property details route

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,12 +3,18 @@ import LandingPage from "./views/LandingPage.vue";
 import RealEstateListings from "./views/RealEstateListings.vue";
 import About from "./views/About.vue";
 import Manage from "./views/management/Manage.vue";
+import PropertyDetails from "./views/details/PropertyDetails.vue";
 
 const routes = [
   { path: "/", component: LandingPage },
   { path: "/real-estate-listings", component: RealEstateListings },
   { path: "/about-us", component: About },
   { path: "/management", component: Manage },
+  {
+    path: "/property/:id",
+    name: "property-details",
+    component: PropertyDetails,
+  },
 ];
 
 const router = createRouter({
